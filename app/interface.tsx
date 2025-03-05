5ef22a8a64036097fcbb91167f266a40e9ee2e6a
waleedmajeed001 <waleedmajeed630@gmail.com>
2025-03-05 09:38:23 +0500

    e

--- app/interface.tsx
+++ app/interface.tsx
@@ -1,19 +1,19 @@
 export interface simplifiedProduct {
-    _id: string;
-    imageUrl: string;
-    price: number;
-    slug: string;
-    categoryName: string;
-    name: string;
-  }
-  
-  export interface fullProduct {
-    _id: string;
-    images: any;
-    price: number;
-    slug: string;
-    categoryName: string;
-    name: string;
-    description: string;
-    price_id: string;
-  }
+  _id: string;
+  imageUrl: string;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+}
+
+export interface fullProduct {
+  _id: string;
+  images: { asset: { url: string } }[]; // Corrected type instead of 'any'
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+  description: string;
+  price_id: string;
+}
--- app/lib/sanity.tsx
+++ app/lib/sanity.tsx
@@ -1,15 +1,17 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+import { ImageUrlBuilder } from "sanity";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-  projectId: "cxsiq338",
+  projectId: "dsnnj0hv",
   dataset: "production",
-  apiVersion: "2025-03-05",
+  apiVersion: "2025-03-04",
   useCdn: true,
 });
 
-const builder = imageUrlBuilder(client);
+const builder: ImageUrlBuilder = imageUrlBuilder(client);
 
-export function urlFor(source: any) {
+export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
